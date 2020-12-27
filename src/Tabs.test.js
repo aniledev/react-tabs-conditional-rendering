15112652e693e83aa792e9da1b7f36621a5359f1
aniledev <elinamcgill@gmail.com>
2020-12-26 22:54:31 -0500

    test: write snapshot test for Tabs component

--- src/Tabs.test.js
+++ src/Tabs.test.js
@@ -3,11 +3,36 @@ import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
 import Tabs from "./Tabs";
 
-// start with a smoke test inside a describe block
 describe("Tabs Component", () => {
+  // array of objects, each with a name and content property
+  const tabsProp = [
+    {
+      name: "First tab",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.",
+    },
+    {
+      name: "Second tab",
+      content:
+        "Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      name: "Third tab",
+      content:
+        "Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.",
+    },
+  ];
+
+  // start with a smoke test inside a describe block
   it("should render without errors", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Tabs />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  //snapshot test using react-test-renderer
+  it("should render the first tab by default", () => {
+    const tree = renderer.create(<Tabs tabs={tabsProp} />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
