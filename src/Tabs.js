2a4babba1a93befffeede33f6be5f24918159672
aniledev <elinamcgill@gmail.com>
2020-12-26 22:23:22 -0500

    feat: fix test error by creating default props for Tabs component

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default class Tabs extends React.Component {
+  static defaultProps = { tabs: [] };
+
   render() {
     // mapp off the tabs array that was passed from App to make an array of buttons
     const buttons = this.props.tabs.map((tab, index) => (
