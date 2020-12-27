b400b5d08425ab665b31af6db9a556256466025b
aniledev <elinamcgill@gmail.com>
2020-12-26 22:20:56 -0500

    feat: use map array method to create buttons based off props

--- src/Tabs.js
+++ src/Tabs.js
@@ -2,6 +2,10 @@ import React from "react";
 
 export default class Tabs extends React.Component {
   render() {
-    return <div></div>;
+    // mapp off the tabs array that was passed from App to make an array of buttons
+    const buttons = this.props.tabs.map((tab, index) => (
+      <button key={index}>{tab.name}</button>
+    ));
+    return <div>{buttons}</div>;
   }
 }
